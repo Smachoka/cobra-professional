6eae6f81bc545db725138045045ee781dcae3f84
Smachoka <silvanusmachoka@gmail.com>
2024-12-03 09:59:12 +0300

    added js

--- script.js
+++ script.js
@@ -6,20 +6,3 @@ function toggleNavLinks() {
     links.style.display = "block"; // Expand
   }
 }
-
-document.addEventListener("DOMContentLoaded", () => {
-  const services = document.querySelectorAll(".service");
-
-  const revealOnScroll = () => {
-    const windowHeight = window.innerHeight;
-    services.forEach((service) => {
-      const serviceTop = service.getBoundingClientRect().top;
-      if (serviceTop < windowHeight - 50) {
-        service.classList.add("show");
-      }
-    });
-  };
-
-  window.addEventListener("scroll", revealOnScroll);
-  revealOnScroll(); // Initial check in case some elements are in view at the start
-});
